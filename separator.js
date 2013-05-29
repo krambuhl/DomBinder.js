7a43813ab5eeb6d7cae7cb0a504720305751a49f
Evan Krambuhl <evan.krambuhl@gmail.com>
2013-05-28 21:14:45 -0600

    Update and rename domBinder.js to separator.js

--- separator.js
+++ separator.js
@@ -1,23 +1,23 @@
 (function(doc, win) {
     var root = this;
 
-    var DomBinder = function DomBinder () {
+    var Separator = function () {
         this.bindings = {};
         this.templates = {};
     }
 
-    this.dom = new DomBinder(); // register helper to window
+    this.Separator = this.Sep = new Separator(); // register helper to window
                                 // remove if you want
 
-    DomBinder.prototype.register = function (name, func) {
+    Separator.prototype.register = function (name, func) {
         this.bindings[name] = func;
     }
 
-    DomBinder.prototype.set = function () {
+    Separator.prototype.set = function () {
         var args = Array.prototype.slice.call(arguments),
             func = args.shift();
 
         this.bindings[func].apply(this, args)
     }
 
-})(document, window)
+}).call(this, document, window);
